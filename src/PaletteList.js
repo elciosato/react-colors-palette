065ad2d24e15cbaf116ac388d861166677837b9d
Elcio Yoshiaki Sato <elciosato@gmail.com>
2022-12-06 22:49:08 +0100

    Start Styling palettelist withstyles

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,12 +1,51 @@
+import { withStyles } from "@mui/styles";
 import MiniPalette from "./MiniPalette";
 
-export default function PaletteList(props) {
+const styles = {
+  root: {
+    backgroundColor: "blue",
+    height: "100%",
+    display: "flex",
+    alignItems: "flex-start",
+    justifyContent: "center",
+  },
+  container: {
+    width: "50%",
+    display: "flex",
+    alignItems: "flex-start",
+    flexDirection: "column",
+    flexWrap: "wrap",
+  },
+  nav: {
+    display: "flex",
+    width: "100%",
+    justifyContent: "space-between",
+    color: "white",
+  },
+  palettes: {
+    boxSizing: "border-box",
+    width: "100%",
+    display: "grid",
+    gridTemplateColumns: "repeat(3, 30%)",
+    gridGap: "5%",
+  },
+};
+function PaletteList(props) {
+  const { palettes, classes } = props;
   return (
-    <div>
-      <h1>React Colors</h1>
-      {props.palettes.map((palette) => (
-        <MiniPalette key={palette.id} {...palette} />
-      ))}
+    <div className={classes.root}>
+      <div className={classes.container}>
+        <nav className={classes.nav}>
+          <h1>React Colors</h1>
+        </nav>
+        <div className={classes.palettes}>
+          {palettes.map((palette) => (
+            <MiniPalette key={palette.id} {...palette} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
+
+export default withStyles(styles)(PaletteList);
