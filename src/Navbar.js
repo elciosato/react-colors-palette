c74feba916eb4957d22d93810391a55e1e2d6da9
Elcio Yoshiaki Sato <elciosato@gmail.com>
2022-12-06 15:59:28 +0100

    Add Snackbar/Material UI

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,22 +4,42 @@ import "rc-slider/assets/index.css";
 import "./Navbar.css";
 import Select from "@mui/material/Select";
 import MenuItem from "@mui/material/MenuItem";
+import Snackbar from "@mui/material/Snackbar";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
 
 export default class Navbar extends Component {
   constructor(props) {
     super(props);
     this.state = {
       format: props.format,
+      open: false,
     };
     this.changeFormatHandler = this.changeFormatHandler.bind(this);
+    this.closeSnackbarHandler = this.closeSnackbarHandler.bind(this);
   }
 
   changeFormatHandler(e) {
-    this.setState({ format: e.target.value });
+    this.setState({ format: e.target.value, open: true });
     this.props.onChangeFormat(e.target.value);
   }
 
+  closeSnackbarHandler() {
+    this.setState({ open: false });
+  }
   render() {
+    const action = (
+      <React.Fragment>
+        <IconButton
+          size="small"
+          aria-label="close"
+          color="inherit"
+          onClick={this.closeSnackbarHandler}
+        >
+          <CloseIcon fontSize="small" />
+        </IconButton>
+      </React.Fragment>
+    );
     return (
       <nav className="Navbar">
         <div className="Navbar-logo">
@@ -49,6 +69,13 @@ export default class Navbar extends Component {
             <MenuItem value="rgba">RGBA - rgba(255, 255, 255, 1.0)</MenuItem>
           </Select>
         </div>
+        <Snackbar
+          open={this.state.open}
+          autoHideDuration={5000}
+          onClose={this.closeSnackbarHandler}
+          message={`Format has been changed to [${this.state.format.toUpperCase()}]`}
+          action={action}
+        />
       </nav>
     );
   }
